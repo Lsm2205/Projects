cdf95cb182256feeb36fc6c184f328341cfe9491
lsm <lsm23@yandex.ru>
2021-04-13 00:32:23 +0300

    add files

---
+++ Lession_IV/main4.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+
+  
+
+int main () {
+   // 1  задание 4 урока                                      
+  const int a  = 10, b = 20, c = 30, d = 40;   
+ 
+    std::cout << a * (b + (static_cast<float>(c) / d));
+    std::cout << "\n";
+    
+
+    return 0;
+}     

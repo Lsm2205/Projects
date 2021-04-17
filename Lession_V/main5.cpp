eb7baf335e5ac57f5f9c9a74c222b32bd3f6ea2d
lsm <lsm23@yandex.ru>
2021-04-17 16:54:26 +0300

    update files

--- Lession_V/main5.cpp
+++ Lession_V/main5.cpp
@@ -1,5 +1,9 @@
  #include <iostream>
+ #include <string>
 
+ using std::cout;
+ using std::cin;
+ using std::endl;
   
 
 int main () {
@@ -8,10 +12,23 @@ int main () {
    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. Написать функцию, 
    заменяющую в принятом массиве 0 на 1, 1 на 0 (** без применения 
    if-else, switch, () ? :);
-
    */                                   
-  
+  int arr[2];
 
+        // Заполняем массив с клавиатуры
+        for (int i = 0; i < 2; i++) {
+            std::cout << "[" << i + 1 << "]" << ": ";
+            cin >> arr[i];
+        }
 
-    return 0;
+        // И выводим заполненный массив.
+        std::cout << "\n Массив: ";
+
+        for (int i = 0; i < 2; ++i) {
+            std::cout << arr[i] << " ";
+        }
+
+        cout << endl;
+
+        return 0;
 }     

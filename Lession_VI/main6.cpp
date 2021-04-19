63dbf94a4a2bed01f2e7854a4581ebdbb9fe0cd5
lsm <lsm23@yandex.ru>
2021-04-19 23:30:26 +0300

    update files

--- Lession_VI/main6.cpp
+++ Lession_VI/main6.cpp
@@ -1,6 +1,7 @@
  #include <iostream>
  #include <fstream>
-  
+ #include <cstring>
+using namespace std;
 
 int main () {
    /* 1 задание 6 урока
@@ -8,9 +9,23 @@ int main () {
    примерно по 50-100 символов в каждом (особого значения не имеет);
    */                                   
  
+   FILE* f = fopen("files_1.txt", "w");
+  
+   fclose(f);
+   
+   std::ofstream out;          
+    out.open("files_1.txt"); 
+    
+     
+        cout<<"Введите текст в files_1.txt:";
+           cin.getline(a);
+        //f<<a;
+       out << &a << std::endl;
 
-
-
-
+     
+    //std::cout << "End of program" << std::endl;
+    
+    //l.close();
+    //system("pause");
     return 0;
 }     

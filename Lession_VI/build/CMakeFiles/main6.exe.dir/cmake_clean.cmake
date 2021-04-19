b00fd61876394329c95867197c1353f6d2371de5
lsm <lsm23@yandex.ru>
2021-04-19 21:10:03 +0300

    add files

---
+++ Lession_VI/build/CMakeFiles/main6.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main6.exe.dir/main6.cpp.o"
+  "main6.exe"
+  "main6.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main6.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

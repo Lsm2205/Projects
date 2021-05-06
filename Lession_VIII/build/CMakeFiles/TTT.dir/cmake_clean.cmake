ed12c8896081a55d652ddafe2148eafa8648c381
lsm <lsm23@yandex.ru>
2021-05-06 18:55:13 +0300

    add files

---
+++ Lession_VIII/build/CMakeFiles/TTT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TTT.dir/main8.cpp.o"
+  "TTT"
+  "TTT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TTT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

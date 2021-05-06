f5db716fa5571316ac8e7fe065fb381473361e31
lsm <lsm23@yandex.ru>
2021-05-06 18:01:16 +0300

    update files

--- Lession_VI/main6.cpp
+++ Lession_VI/main6.cpp
@@ -4,9 +4,23 @@
  #include <algorithm>
  #include <vector>
  #include <iterator>
-  //using namespace std;
+  using namespace std;
 
-  /* void create_random_file(const char* filesname){
+  void readingFile(string[], ifstream &);
+  
+  void readingFile(string[], ifstream &infile)
+    {   
+    char next;
+    infile.get(next);
+        while (!infile.eof())  
+        {
+            cout << next;    
+            infile.get(next);
+        }
+     } 
+
+ /*
+  void create_random_file(const char* filesname){
       std::ofstream stream {filesname};
       for (size_t i = 0; i < 50; i++)
    {
@@ -15,15 +29,15 @@
       stream << c;
    }
    }
-*/
-int main (int argv,char** argc) {
+   */
+int main () {
    /* 1 задание 6 урока
    Написать программу, которая создаст два текстовых файла, 
    примерно по 50-100 символов в каждом (особого значения не имеет);
    */   
 
       //create_random_file("files1.txt");
-     // reate_random_file("files2.txt");
+      //reate_random_file("files2.txt");
 
 
 
@@ -52,53 +66,46 @@ int main (int argv,char** argc) {
      Написать функцию, «склеивающую» эти файлы, предварительно буферизуя 
      их содержимое в динамически выделенный сегмент памяти нужного размера.
      */
-    if(argv<3)
+
+     string header1[50], header2[50] ; 
+
+    int size1, size2, size3;
+
+    ifstream Fin, Fin2;
+    ofstream Fout;
+
+    Fin.open("file1.txt");   
+    Fin2.open("file2.txt");
+    
+    if (!Fin || !Fin2 )    
     {
-    std::cout << "Неправильные входные параметры" << std::endl;
-      return -1;
+        std::cout << "Не удалось открыть файл.\n";
+        cin.ignore();
+        return 1;
     }
 
-
-
-  //читаем файл
-  std::fstream file1 ( "file1.txt" );
-  std::fstream file2 ( "file2.txt" );
-  file1.open(argc[1]);
-  file2.open(argc[2]);
-
-
-
-  if((!file1)||(!file2))
+    Fout.open("files1.txt");  
+    if (!Fout)
     {
-      std::cout << "Не удается открыть один из файлов" << std::endl;
+        std::cout << "Не удалось открыть файл.\n";
+        cin.ignore();
+        return 2;
     }
 
+    std::cout << "Редактирование....\n" << endl;
+    cin.ignore();
+
+    std::cout << "Файл заголовков 1:\n" << endl;
+    readingFile(header1, Fin);  
+    cout << endl << endl;
+    std::cout << "Файл заголовков 2:\n" << endl;
+    readingFile(header2, Fin2); 
+    cout << endl << endl;
+    
+
+    cin.ignore();
 
 
-  std::istream_iterator<std::string> in1(file1);
-  std::istream_iterator<std::string> in2(file2);
-  std::istream_iterator<std::string> eof1;
-  std::istream_iterator<std::string> eof2;
-
-  std::vector<std::string> vector1(in1,eof1);
-  std::vector<std::string> vector2(in2,eof2);
-
-
-  std::vector<std::string> names;
-
-
-  std::copy(vector1.begin(),vector1.end(),back_inserter(names));
-  std::copy(vector2.begin(),vector2.end(),back_inserter(names));
-
-
-  //std::copy(names.begin(),names.end(),std::ostream_iterator<std::string>(std::cout," "));
-
-  std::sort(names.begin(),names.end());
-  auto it=std::unique(names.begin(),names.end());
-
-  names.erase(it);
-
-  std::copy(names.begin(),names.end(),std::ostream_iterator<std::string>(std::cout," "));
 
     return 0;
 }     
